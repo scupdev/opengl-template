13cbedac234497cb8ed21f6923e5532cb2c757a1
scup <110328123+scupdev@users.noreply.github.com>
2022-09-16 21:59:00 +0530

    Delete build directory

--- build/game/CMakeFiles/game.dir/src/add.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-game/CMakeFiles/game.dir/src/add.cpp.obj: \
- C:\Programming\opengl-template\game\src\add.cpp \
- C:\Programming\opengl-template\game\src\include/add.h
--- build/src/CMakeFiles/src.dir/add.cpp.obj.d
+++
@@ -1,3 +0,0 @@
-src/CMakeFiles/src.dir/add.cpp.obj: \
- C:\Programming\opengl-template\src\add.cpp \
- C:\Programming\opengl-template\src\include/add.h
