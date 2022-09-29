6d95cc439ec4efa321fcd2da38f199e508907419
scupdev <ot.gaming132@gmail.com>
2022-09-29 11:27:06 +0530

    rework

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
 #include <glad/glad.h>
-#include "include/display.hpp"
 
 int main() {
-    Display display(800, 600, "Hello Opengl");
-
-    while (!display.isClosed()) {
-        display.clear(1.f,0.f,1.f,0.5f); 
-        display.update();
-    }
-
+    std::cout << "br" << "\n";
     return 0;
 }
